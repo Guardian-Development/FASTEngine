00901b3271ac70de5de35b8397b7a47bf88dfe05
joe honour <joe.honour@bjss.com>
2020-01-20 14:41:57 -0500

    * refactoring decimal field

--- internal/fast/field/field.go
+++ internal/fast/field/field.go
@@ -173,61 +173,30 @@ func (field Int64) GetTagId() uint64 {
 	return field.FieldDetails.ID
 }
 
-// TODO: if we refactored deseralise to return the value itself, we can use exponent operations to just be int32 fields instead, which would be better
-// TODO: once refactores to hold filed rather than operations this all becomes a lot simpler. do this next.
-// TODO: may also need to refactor constant operation to store a fix.Value so can represent decimal correctly (fix.Value will need a decimal type)
 type Decimal struct {
-	FieldDetails      Field
-	ExponentOperation operation.Operation
-	MantissaOperation operation.Operation
+	FieldDetails  Field
+	ExponentField Int32
+	MantissaField Int64
 }
 
 func (field Decimal) Deserialise(inputSource *bytes.Buffer, pMap *presencemap.PresenceMap) (fix.Value, error) {
-	if field.ExponentOperation.ShouldReadValue(pMap, field.FieldDetails.Required) {
-		var exponentValue value.Value
-		var err error
-
-		if field.FieldDetails.Required {
-			exponentValue, err = fast.ReadInt32(inputSource)
-		} else {
-			exponentValue, err = fast.ReadOptionalInt32(inputSource)
-		}
-		transformedExponentValue, err := field.ExponentOperation.Apply(exponentValue)
-
-		if err != nil {
-			return nil, err
-		}
-
-		return field.buildDecimalFromExponent(inputSource, transformedExponentValue)
-	}
-
-	value, err := field.ExponentOperation.GetNotEncodedValue()
-
+	exponentValue, err := field.ExponentField.Deserialise(inputSource, pMap)
 	if err != nil {
 		return nil, err
 	}
-	
-	return field.buildDecimalFromExponent(inputSource, value)
-}
-
-func (field Decimal) buildDecimalFromExponent(inputSource *bytes.Buffer, transformedExponentValue fix.Value) (fix.Value, error) {
-	switch transformedExponentValue.(type) {
+	switch exponentValue.(type) {
 	case fix.NullValue:
 		return fix.NullValue{}, nil
 	case fix.RawValue:
-		exponentValue := transformedExponentValue.Get().(int32)
-		mantissaValue, err := fast.ReadInt64(inputSource)
-		transformedMantissaValue, err := field.MantissaOperation.Apply(mantissaValue)
-
+		mantissaValue, err := field.MantissaField.Deserialise(inputSource, pMap)
 		if err != nil {
 			return nil, err
 		}
-
-		decimalValue := math.Pow(10, float64(exponentValue)) * float64(transformedMantissaValue.Get().(int64))
+		decimalValue := math.Pow(10, float64(exponentValue.Get().(int32))) * float64(mantissaValue.Get().(int64))
 		return fix.NewRawValue(decimalValue), nil
 	}
 
-	return fix.NullValue{}, fmt.Errorf("Unsupported exponent type for building decimal: %v", transformedExponentValue)
+	return nil, fmt.Errorf("Exponent value of decimal was not expected type: %v", exponentValue)
 }
 
 func (field Decimal) GetTagId() uint64 {
